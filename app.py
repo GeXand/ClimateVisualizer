7149b93944895ba8f132c946f5efb631a01ac061
GeXand <xander.q.ge@gmail.com>
2021-03-06 21:29:16 -0600

    Additional Page + testing

--- app.py
+++ app.py
@@ -7,23 +7,28 @@ from newspaper import Article
 app = Flask(__name__)
 
 def extract(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    article.nlp()
-    return {"summary": article.summary, "keywords": article.keywords}
-
+    try:
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
+        return {"summary": article.summary, "keywords": article.keywords}
+    except Exception as e:
+        print(e)
+        return {"summary": "", "keywords": []}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/energy')
+def energy():
+    return render_template('energy.html')
+
 @app.route('/pred')
 def pred():
-    news = getNews("carbon emissions OR greenhouse gas OR co2 emissions", max_articles=20)
-    curArticle = news[4]
-    article_obj = extract(curArticle['href'])
-    return render_template('pred.html', title=curArticle['title'], link=curArticle['href'],
+    article_obj = extract('https://www.theguardian.com/environment/2020/aug/31/australias-big-polluters-required-to-offset-just-12-of-greenhouse-gas-emissions-in-past-year%27')
+    return render_template('pred.html', title='pee pee', link='poo poo',
                            summary=article_obj['summary'], keywords=', '.join(article_obj['keywords']))
 
 
